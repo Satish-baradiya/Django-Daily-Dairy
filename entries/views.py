c4f514ce3f4a4a809f68a930d4848a44b371e1be
Satish-baradiya <baradiyasatish@gmail.com>
2022-01-07 22:22:24 +0530

    Intial Setup

---
+++ entries/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from . models import Entry
+
+# Create your views here.
+
+
+def index(request):
+    entries = Entry.objects.all().order_by("-date_created")
+    context = {'entries': entries}
+    return render(request,'entries/entry.html',context)
+
+def content(request,pk_test):
+    entry = Entry.objects.get(id=pk_test)
+    context = {'entry':entry}
+    return render(request,'entries/content.html',context)
+
+'''
+def entry(request):
+    if request.method == "POST":
+        title = request.POST['title']
+        content = request.POST['content']'''
+
+
+
+    
+
