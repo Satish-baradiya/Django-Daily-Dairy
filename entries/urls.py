14e19879bd39c29f65980435785e59ae21bbacc3
Satish-baradiya <baradiyasatish@gmail.com>
2022-01-08 22:13:39 +0530

    Input Form created

--- entries/urls.py
+++ entries/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+import entries
 
 urlpatterns = [
     path('',views.index,name="index"),
-    path('entry/<int:pk_test>/',views.content,name="content")
+    path('entry/<int:pk_test>/',views.content,name="content"),
+    path('newentry',views.entry,name="entry")
 
 ]
--- entries/views.py
+++ entries/views.py
@@ -7,20 +7,20 @@ from . models import Entry
 def index(request):
     entries = Entry.objects.all().order_by("-date_created")
     context = {'entries': entries}
-    return render(request,'entries/entry.html',context)
+    return render(request, 'entries/entry.html', context)
 
-def content(request,pk_test):
+
+def content(request, pk_test):
     entry = Entry.objects.get(id=pk_test)
-    context = {'entry':entry}
-    return render(request,'entries/content.html',context)
+    context = {'entry': entry}
+    return render(request, 'entries/content.html', context)
 
-'''
+
 def entry(request):
     if request.method == "POST":
         title = request.POST['title']
-        content = request.POST['content']'''
+        content = request.POST['content']
+        date_created = request.POST['date']
 
-
-
-    
+    return render(request,'entries/newentry.html')
 
